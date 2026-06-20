a37a8bffea5fdd164373605bb89bcb9fa3c5523b
Saloni <sagarwal9475@gmail.com>
2026-06-20 23:32:56 +0530

    Impoved chunking service

--- backend/app/services/chunk_service.py
+++ backend/app/services/chunk_service.py
@@ -1,17 +1,33 @@
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+# def chunk_text(
+#         text: str,
+#         chunk_size: int = 500,
+#         overlap: int = 100
+# ):
+#     chunks = []
+
+#     start = 0
+
+#     while start < len(text):
+#         end = start + chunk_size
+
+#         chunks.append(text[start:end])
+
+#         start += chunk_size - overlap
+
+#     return chunks
+
+
+
 def chunk_text(
         text: str,
-        chunk_size: int = 500,
-        overlap: int = 100
+        chunk_size: int = 800,
+        overlap: int = 150
 ):
-    chunks = []
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=chunk_size,
+        chunk_overlap=overlap
+    )
 
-    start = 0
-
-    while start < len(text):
-        end = start + chunk_size
-
-        chunks.append(text[start:end])
-
-        start += chunk_size - overlap
-
-    return chunks
+    return splitter.split_text(text)
